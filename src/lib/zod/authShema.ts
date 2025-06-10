43b9d438a8adde73df8175fd1db14d467a144b2f
SebasDev <sebastian.ramirez@blackbirdlabs.com.co>
2025-06-10 15:30:27 -0500

    feat(Auth): Creacion del checkbox de tipo de usuario

--- src/lib/zod/authShema.ts
+++ src/lib/zod/authShema.ts
@@ -2,5 +2,8 @@ import {z} from "zod";
 
 export const LoginSchema = z.object({
     username: z.string().min(1, "El usuario es requerido"),
-    password: z.string().min(8, "La contraseña debe tener al menos 8 caracteres")
+    password: z.string().min(8, "La contraseña debe tener al menos 8 caracteres"),
+    role: z.enum(["1", "2"], {
+        required_error: "El rol es requerido",
+    })
 });
