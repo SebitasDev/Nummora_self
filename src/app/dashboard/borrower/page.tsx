82b919cf072918b6e7a70a82d332de3141a7a885
Karmejares <jamesmoncada0120@gmail.com>
2025-06-10 13:55:22 -0500

    borrower/borrower-visual-dashboard

--- src/app/dashboard/borrower/page.tsx
+++ src/app/dashboard/borrower/page.tsx
@@ -1,5 +1,5 @@
 'use client'
 
 import {DashboardBorrower} from "@/components/layouts/DashboardBorrower";
-
+//Change to commit no mas :P
 export default function BorrowerPage() { return <DashboardBorrower/>; }
