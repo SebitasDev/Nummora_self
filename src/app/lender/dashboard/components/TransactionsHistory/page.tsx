548a92caba3d3b9dadb6faee14f69092a4de8363
Karmejares <jamesmoncada0120@gmail.com>
2025-07-10 13:27:31 -0500

    history template

---
+++ src/app/lender/dashboard/components/TransactionsHistory/page.tsx
@@ -0,0 +1,5 @@
+import TransactionsHistory from "@/app/lender/dashboard/components/TransactionsHistory/TransactionsHistory";
+
+export default function TransactionsHistoryPage() {
+  return <TransactionsHistory />;
+}
